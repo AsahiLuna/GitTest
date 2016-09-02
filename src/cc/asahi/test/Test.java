4c45bf110128d81579dcd9203b043c1d8ebce1d9
张泽旭 <542792296@qq.com>
2016-09-02 21:25:11 +0800

    8

--- src/cc/asahi/test/Test.java
+++ src/cc/asahi/test/Test.java
@@ -7,8 +7,9 @@ public class Test {
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		printN(100000);
 	}
 	public static void printN(int n){
-		System.out.println(8);
+		System.out.println(8) ;
 	}
 }
